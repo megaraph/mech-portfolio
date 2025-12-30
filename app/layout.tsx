a8eb830b4ab8f89ceee964e38539c0b7ed5bb8b5
megaraph <47447126+megaraph@users.noreply.github.com>
2025-12-31 03:43:07 +0800

    add: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,15 @@ const monoFont = JetBrains_Mono({
 export const metadata: Metadata = {
     title: "Raphael Murillo | Mechanical Engineer",
     description: "Mechanical Engineering Portfolio",
+    icons: {
+        icon: [
+            { url: "/favicon.ico" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        ],
+        apple: "/apple-touch-icon.png",
+    },
+    manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
